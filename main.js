12916f2e08a4b04cefc295a690f91aab58f1228f
Juan Bocanegra <jbocanegrae@unal.edu.co>
2024-07-18 20:52:00 -0500

    ejercicios bucles hasta punto 5

---
+++ main.js
@@ -0,0 +1,77 @@
+// Punto 1
+
+// let num = prompt("Ingresa un numero");
+
+// for (let i = 0; i <= 10; i++) {
+//     const multiplo = num * i;
+//     console.log(multiplo); 
+// }
+
+// Punto 2
+
+// let suma = 0
+// let numero
+
+// do {
+//     numero = Number(prompt("Ingresa un numero"))
+//     if (numero !== 0) {
+//         suma += numero
+//         console.log(suma);
+//     }
+// } while (numero !== 0)
+    
+// console.log("La suma es " + suma)
+
+// Punto 3
+// let i = 0
+// let gano = false
+// let numeroIncognita = 60
+
+// while (gano === false) {
+//     i++
+//     let numeroIngresado = parseInt(prompt("Ingresa un numero"))
+//     if (numeroIngresado === numeroIncognita) {
+//         gano = true
+//         alert("Felicidades, lo lograste en " + i + " intentos")
+//         break;
+//     }
+//     if (numeroIngresado < numeroIncognita) {
+//         alert("El numero es mayor");
+//     } else {
+//        alert("El numero es menor");
+//     }
+    
+// }
+
+// Punto 4
+
+// let numero = parseInt(prompt("Ingresa un numero"));
+
+// for (let i = 2; i < numero; i++) {
+//     if (numero % i === 0) {
+//         alert("El numero "+ numero +" NO es primo");
+//         break;
+//     }
+//     if (i === numero - 1) {
+//         alert("El numero "+ numero +" es primo");
+//     }
+// }
+
+// Punto 5
+
+// let numero = parseInt(prompt("Ingresa un numero"));
+
+
+// for (let i = 1; i <= numero; i++){
+//     let divisores = []
+//     if (numero % i === 0) {
+//         divisores.push(i)
+//     } else {
+//         continue
+//     }
+//     console.log(divisores);
+// }
+
+// Punto 6
+
+
